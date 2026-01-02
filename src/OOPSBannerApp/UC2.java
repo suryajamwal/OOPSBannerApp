07e46d0c600b409f80a8da37d6028321d09bd986
surya <suryajamwal@143.com>
2026-01-02 11:15:55 +0530

    UC2 Code updated

--- src/OOPSBannerApp/UC2.java
+++ src/OOPSBannerApp/UC2.java
@@ -14,15 +14,14 @@ public class UC2 {
                 "   ***       ***      **        ****** "
         };
 
-        // enhanced for-loop traversal
+
         for (String line : bannerLines) {
-            // traversal demonstration
+
         }
 
-        // join lines using String.join()
         String banner = String.join("\n", bannerLines);
 
-        // traditional for-loop for printing
+
         for (int i = 0; i < 1; i++) {
             System.out.println(banner);
         }
